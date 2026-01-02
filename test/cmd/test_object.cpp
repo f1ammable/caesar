1a8a2be75f73dfdddc945649c3bbbb15cdd0c73c
Gabriel <d9rect@pm.me>
2026-01-02 22:40:49 +0000

    add tests for `Object` operators

--- test/cmd/test_object.cpp
+++ test/cmd/test_object.cpp
@@ -6,79 +6,164 @@
 #include "object.hpp"
 #include "test_helpers.hpp"
 
-TEST_CASE("Test binary operation", "[object][operator]") {
-  auto lhs = GENERATE(Object{5.0}, Object{true}, Object{"hello"},
-                      Object{std::monostate{}});
+// This whole file is ugly but it works and I can't find it a better way to do
+// it
 
-  auto rhs = GENERATE(Object{3.0}, Object{false}, Object{" world"},
-                      Object{std::monostate{}});
+TEST_CASE("Test arithmetic operators between all type combinations",
+          "[object][operator][arithmetic]") {
+  auto [op_name, op, lhs, rhs, should_succeed, expected_type] = GENERATE(
+      table<std::string, std::function<Object(const Object&, const Object&)>,
+            Object, Object, bool, std::string>({
+          // Addition
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{5.0}, Object{3.0}, true, "double"},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{5.0}, Object{true}, true, "double"},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{true}, Object{3.0}, true, "double"},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{true}, Object{false}, true, "double"},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{"hello"}, Object{" world"}, true, "string"},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{5.0}, Object{"hello"}, false, ""},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{"hello"}, Object{5.0}, false, ""},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{true}, Object{"hello"}, false, ""},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{"hello"}, Object{true}, false, ""},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{std::monostate{}}, Object{5.0}, false, ""},
+          {"+", [](const Object& l, const Object& r) { return l + r; },
+           Object{5.0}, Object{std::monostate{}}, false, ""},
 
-  bool lhs_is_num =
-      std::holds_alternative<double>(lhs) || std::holds_alternative<bool>(lhs);
-  bool rhs_is_num =
-      std::holds_alternative<double>(rhs) || std::holds_alternative<bool>(rhs);
-  bool lhs_is_str = std::holds_alternative<std::string>(lhs);
-  bool rhs_is_str = std::holds_alternative<std::string>(rhs);
+          // Subtraction
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{5.0}, Object{3.0}, true, "double"},
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{5.0}, Object{true}, true, "double"},
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{true}, Object{false}, true, "double"},
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{"hello"}, Object{"world"}, false, ""},
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{5.0}, Object{"hello"}, false, ""},
+          {"-", [](const Object& l, const Object& r) { return l - r; },
+           Object{std::monostate{}}, Object{5.0}, false, ""},
 
-  if (lhs_is_num && rhs_is_num) {
-    Object res = lhs + rhs;
-    REQUIRE(std::holds_alternative<double>(res));
-  } else if (lhs_is_str && rhs_is_str) {
-    Object res = lhs + rhs;
-    REQUIRE(std::holds_alternative<std::string>(res));
+          // Multiplication
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{5.0}, Object{3.0}, true, "double"},
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{5.0}, Object{true}, true, "double"},
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{true}, Object{false}, true, "double"},
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{"hello"}, Object{"world"}, false, ""},
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{5.0}, Object{"hello"}, false, ""},
+          {"*", [](const Object& l, const Object& r) { return l * r; },
+           Object{std::monostate{}}, Object{5.0}, false, ""},
+
+          // Division
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{6.0}, Object{3.0}, true, "double"},
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{5.0}, Object{true}, true, "double"},
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{true}, Object{true}, true, "double"},
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{"hello"}, Object{"world"}, false, ""},
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{5.0}, Object{"hello"}, false, ""},
+          {"/", [](const Object& l, const Object& r) { return l / r; },
+           Object{std::monostate{}}, Object{5.0}, false, ""},
+      }));
+
+  if (should_succeed) {
+    Object res = op(lhs, rhs);
+    if (expected_type == "double") {
+      REQUIRE(std::holds_alternative<double>(res));
+    } else if (expected_type == "string") {
+      REQUIRE(std::holds_alternative<std::string>(res));
+    }
   } else {
-    auto captured = helpers::captureStream(
-        std::cerr, [](const Object& l, const Object& r) { return l + r; }, lhs,
-        rhs);
-    REQUIRE(captured.find("Unsupported operand types") != std::string::npos);
+    auto captured = helpers::captureStream(std::cerr, op, lhs, rhs);
+    bool has_string_error =
+        captured.find(std::format("Cannot apply operator {} to strings",
+                                  op_name)) != std::string::npos;
+    bool has_unsupported_error =
+        captured.find("Unsupported operand types") != std::string::npos;
+    REQUIRE((has_string_error || has_unsupported_error));
   }
 }
 
-TEST_CASE("Test operator behaviour for string operations",
-          "[object][operator]") {
-  // Omit + operator as its covered by test above
-  auto [op_name, op] = GENERATE(
-      table<std::string, std::function<Object(const Object&, const Object&)>>(
-          {{"-",
-            [](const Object& lhs, const Object& rhs) { return lhs - rhs; }},
-           {"*",
-            [](const Object& lhs, const Object& rhs) { return lhs * rhs; }},
-           {"/",
-            [](const Object& lhs, const Object& rhs) { return lhs / rhs; }}}));
+TEST_CASE("Test comparison operators between all type combinations",
+          "[object][operator][comparison]") {
+  auto [op_name, op, lhs, rhs, should_succeed] = GENERATE(
+      table<std::string, std::function<bool(const Object&, const Object&)>,
+            Object, Object, bool>({
+          // Less than
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{5.0}, Object{3.0}, true},
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{5.0}, Object{true}, true},
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{true}, Object{false}, true},
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{"hello"}, Object{"world"}, false},
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{5.0}, Object{"hello"}, false},
+          {"<", [](const Object& l, const Object& r) { return l < r; },
+           Object{std::monostate{}}, Object{5.0}, false},
 
-  Object lhs = Object{"hello"};
-  Object rhs = Object{"rhs"};
-  auto captured = helpers::captureStream(std::cerr, op, lhs, rhs);
-  REQUIRE(captured.find(std::format("Cannot apply operator {} to strings",
-                                    op_name)) != std::string::npos);
-}
+          // Greater than
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{5.0}, Object{3.0}, true},
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{5.0}, Object{true}, true},
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{true}, Object{false}, true},
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{"hello"}, Object{"world"}, false},
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{5.0}, Object{"hello"}, false},
+          {">", [](const Object& l, const Object& r) { return l > r; },
+           Object{std::monostate{}}, Object{5.0}, false},
 
-TEST_CASE("Test comparison operators", "[object][operator]") {
-  auto [op_name, op] = GENERATE(
-      table<std::string, std::function<Object(const Object&, const Object&)>>(
-          {{"<",
-            [](const Object& lhs, const Object& rhs) { return lhs < rhs; }},
-           {">",
-            [](const Object& lhs, const Object& rhs) { return lhs > rhs; }},
-           {"<=",
-            [](const Object& lhs, const Object& rhs) { return lhs <= rhs; }},
-           {">=",
-            [](const Object& lhs, const Object& rhs) { return lhs >= rhs; }}}));
+          // Less than or equal to
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{5.0}, Object{3.0}, true},
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{5.0}, Object{true}, true},
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{true}, Object{false}, true},
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{"hello"}, Object{"world"}, false},
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{5.0}, Object{"hello"}, false},
+          {"<=", [](const Object& l, const Object& r) { return l <= r; },
+           Object{std::monostate{}}, Object{5.0}, false},
 
-  auto lhs = GENERATE(Object{5.0}, Object{true}, Object{"hello"},
-                      Object{std::monostate{}});
+          // Greater than or equal to
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{5.0}, Object{3.0}, true},
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{5.0}, Object{true}, true},
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{true}, Object{false}, true},
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{"hello"}, Object{"world"}, false},
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{5.0}, Object{"hello"}, false},
+          {">=", [](const Object& l, const Object& r) { return l >= r; },
+           Object{std::monostate{}}, Object{5.0}, false},
+      }));
 
-  auto rhs = GENERATE(Object{3.0}, Object{false}, Object{" world"},
-                      Object{std::monostate{}});
-
-  bool lhs_is_num =
-      std::holds_alternative<double>(lhs) || std::holds_alternative<bool>(lhs);
-  bool rhs_is_num =
-      std::holds_alternative<double>(rhs) || std::holds_alternative<bool>(rhs);
-
-  if (lhs_is_num && rhs_is_num) {
-    Object res = lhs + rhs;
-    REQUIRE(std::holds_alternative<double>(res));
+  if (should_succeed) {
+    bool result = op(lhs, rhs);
+    REQUIRE((result == true || result == false));
   } else {
     auto captured = helpers::captureStream(std::cerr, op, lhs, rhs);
     REQUIRE(captured.find(std::format(
@@ -87,24 +172,68 @@ TEST_CASE("Test comparison operators", "[object][operator]") {
   }
 }
 
-TEST_CASE("Test equality operators", "[object][operator]") {
-  auto lhs = GENERATE(Object{5.0}, Object{true}, Object{"hello"},
-                      Object{std::monostate{}});
+TEST_CASE("Test equality operators between all type combinations",
+          "[object][operator][equality]") {
+  auto [op_name, op, lhs, rhs, expected_result] = GENERATE(
+      table<std::string, std::function<bool(const Object&, const Object&)>,
+            Object, Object, bool>({
+          // == operator - same types
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{5.0}, Object{5.0}, true},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{true}, Object{true}, true},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{"hello"}, Object{"hello"}, true},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{std::monostate{}}, Object{std::monostate{}}, true},
 
-  auto rhs = GENERATE(Object{5.0}, Object{true}, Object{"hello"},
-                      Object{std::monostate{}});
+          // == operator - different values, same types
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{5.0}, Object{3.0}, false},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{true}, Object{false}, false},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{"hello"}, Object{"world"}, false},
 
-  auto [op_name, op,
-        res] = GENERATE(table<std::string,
-                              std::function<bool(const Object&, const Object&)>,
-                              bool>(
-      {{"==", [](const Object& lhs, const Object& rhs) { return lhs == rhs; },
-        true},
-       {"!=", [](const Object& lhs, const Object& rhs) { return lhs != rhs; },
-        false}}));
+          // == operator - different types
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{5.0}, Object{true}, false},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{5.0}, Object{"hello"}, false},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{true}, Object{"hello"}, false},
+          {"==", [](const Object& l, const Object& r) { return l == r; },
+           Object{std::monostate{}}, Object{5.0}, false},
 
-  if (lhs.index() == rhs.index())
-    REQUIRE(op(lhs, rhs) == res);
-  else
-    REQUIRE(op(lhs, rhs) == !res);
+          // != operator - same types
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{5.0}, Object{5.0}, false},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{true}, Object{true}, false},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{"hello"}, Object{"hello"}, false},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{std::monostate{}}, Object{std::monostate{}}, false},
+
+          // != operator - different values, same types
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{5.0}, Object{3.0}, true},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{true}, Object{false}, true},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{"hello"}, Object{"world"}, true},
+
+          // != operator - different types
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{5.0}, Object{true}, true},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{5.0}, Object{"hello"}, true},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{true}, Object{"hello"}, true},
+          {"!=", [](const Object& l, const Object& r) { return l != r; },
+           Object{std::monostate{}}, Object{5.0}, true},
+      }));
+
+  bool result = op(lhs, rhs);
+  REQUIRE(result == expected_result);
 }
