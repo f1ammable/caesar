299a1a7b63504c75f4a59e24738f9566c900a1a4
Gabriel <d9rect@pm.me>
2026-02-11 11:16:26 +0000

    refactor `Target`

--- src/core/context.hpp
+++ src/core/context.hpp
@@ -4,9 +4,7 @@
 #include <cassert>
 #include <memory>
 
-#include "macho.hpp"
 #include "target.hpp"
-#include "util.hpp"
 
 // NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
 class Context {
@@ -25,17 +23,6 @@ class Context {
     static Context instance;
     return instance;
   }
-
-  void initTarget(std::string path) {
-    // TODO: Add other platforms here
-    switch (getPlatform()) {
-      case PlatformType::MACH:
-        m_target = std::make_unique<Macho>(std::move(std::ifstream(path)),
-                                           std::move(path));
-    }
-    assert(m_target != nullptr && "Target is not supported yet!");
-  }
-
   std::unique_ptr<Target>& getTarget() { return m_target; }
 };
 
