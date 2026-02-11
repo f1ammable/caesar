56a731387546d20225846ce18539f6417827bd9b
Gabriel <d9rect@pm.me>
2026-02-11 11:17:55 +0000

    move `Macho` into own library

--- src/core/macho/macho.hpp
+++ src/core/macho/macho.hpp
@@ -3,13 +3,18 @@
 
 #include <mach-o/loader.h>
 #include <mach-o/swap.h>
+#include <mach/arm/kern_return.h>
+#include <mach/exception_types.h>
+#include <mach/mach_types.h>
 #include <mach/machine.h>
+#include <mach/message.h>
+#include <mach/thread_status.h>
 
 #include <fstream>
 #include <string>
 
-#include "target.hpp"
-#include "util.hpp"
+#include "core/target.hpp"
+#include "core/util.hpp"
 
 struct CpuTypeNames {
   cpu_type_t cpu_type;
@@ -20,14 +25,19 @@ class Macho : public Target {
  public:
   explicit Macho(std::ifstream f, std::string filePath);
   void dump() override;
-  i32 attach(u32 pid) override;
+  i32 attach(i32 pid) override;
   void setBreakpoint(u32 addr) override;
   i32 launch(CStringArray& argList) override;
+  void detach(i32 pid) override;
+  void eventLoop() const override;
 
  private:
   uint32_t m_magic = 0;
   bool m_is_64 = false;
   bool m_is_swap = false;
+  task_t m_task_port = 0;
+  mach_port_t m_exc_port = 0;
+  mach_port_t m_thread_port = 0;
 
   void readMagic() override;
   void is64() override;
@@ -36,8 +46,8 @@ class Macho : public Target {
   template <typename T>
   T loadBytesAndMaybeSwap(uint32_t offset) {
     T buf;
-    m_file->seekg(offset, std::ios::beg);
-    m_file->read(std::bit_cast<char*>(&buf), sizeof(T));
+    m_file.seekg(offset, std::ios::beg);
+    m_file.read(std::bit_cast<char*>(&buf), sizeof(T));
     if (m_is_swap) SwapDescriptor<T>::swap(&buf);
     return buf;
   }
@@ -52,6 +62,9 @@ class Macho : public Target {
        {.cpu_type = CPU_TYPE_ARM64, .cpu_name = "arm64"}}};
   static std::string cpuTypeName(cpu_type_t cpuType);
   void dumpSections(uint32_t offset, uint32_t end);
+  i32 setupExceptionPorts(task_t task);
+
+  void threadSelect();  // Currently only selects main thread
 };
 
 #endif  // CAESAR_MACHO_HPP
---
+++ src/core/macho/ports.hpp
@@ -0,0 +1,18 @@
+#ifndef CAESAR_PORTS_HPP
+#define CAESAR_PORTS_HPP
+
+#include <mach/exception_types.h>
+#include <mach/message.h>
+#include <mach/thread_status.h>
+
+#include <array>
+
+struct MachExcPorts {
+  std::array<exception_mask_t, EXC_TYPES_COUNT> masks{};
+  std::array<mach_port_t, EXC_TYPES_COUNT> ports{};
+  std::array<exception_behavior_t, EXC_TYPES_COUNT> behaviours{};
+  std::array<thread_state_flavor_t, EXC_TYPES_COUNT> flavours{};
+  mach_msg_type_number_t excp_type_count{};
+};
+
+#endif
